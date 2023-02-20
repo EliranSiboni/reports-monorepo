764e815891e96c2646e7dc4be57842da8d2a8d07
Eliran Siboni <eliran.siboni@gmail.com>
2023-02-20 14:35:26 +0000

    feat: add chakra ui dependencies

--- apps/app/pages/_app.tsx
+++ apps/app/pages/_app.tsx
@@ -1,12 +1,17 @@
 import type { AppProps } from "next/app";
 import "@vercel/examples-ui/globals.css";
 import { ApolloProvider } from "@apollo/client";
+import { ChakraProvider } from "@chakra-ui/react";
+
 import { client } from "apollo-client";
+import theme from "theme";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
-      <Component {...pageProps} />
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </ApolloProvider>
   );
 }
